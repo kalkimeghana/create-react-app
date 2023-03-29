e8e140e4dc8a9ce6b939aec3678bda19284c01d0
kalkimeghana <meghanakalki@gmail.com>
2023-03-29 21:10:28 +0530

    Update App.js

--- packages/cra-template/template/src/App.js
+++ packages/cra-template/template/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>This is react application</h1>
+      <h1>This is react asdadpplication</h1>
     </div>
   );
 }
